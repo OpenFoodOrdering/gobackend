4348491439395e1ce8928222887d7be6ed1ceaf4
foehammer127 <foehammer127@gmail.com>
2023-06-03 20:35:48 -0500

    Make Database Work

--- db/db.go
+++ db/db.go
@@ -3,11 +3,11 @@ package db
 import (
 	"context"
 	"log"
-	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var MongoDb mongo.Client
@@ -18,19 +18,19 @@ func Init(url string) {
 	ServerAPI := options.ServerAPI(options.ServerAPIVersion1)
 	ClientOptions := options.Client().ApplyURI(url).SetServerAPIOptions(ServerAPI)
 
+	ctx := context.Background()
 	// Initialize MongoDb Client Connection Pool Using ClientOptions
-	MongoDb, err := mongo.NewClient(ClientOptions)
+	MongoDb, err := mongo.Connect(ctx, ClientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Mongodb_Initial_Connection: ", err)
 	}
 
-	ctx, cancelfunc := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancelfunc()
-	// Ping MongoDb
-	err = MongoDb.Ping(ctx, &readpref.ReadPref{})
-	if err != nil {
-		log.Fatal("Unable to Connect to Mongo Db:", err)
+	// Ping Mongo
+	if err = MongoDb.Database("openfoodordering").RunCommand(ctx, bson.D{primitive.E{Key: "ping", Value: 1}}).Err(); err != nil {
+		log.Fatal("Mongodb:", err)
 	}
+
+	log.Println("MongoDB Connection Successful")
 }
 
 func GetClient() *mongo.Client {
